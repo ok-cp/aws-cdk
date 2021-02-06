6aa78a6dd692aa1160900da2befe5cc5a973a0fc
김범승 <bamdal0615@gmail.com>
2021-02-07 04:32:13 +0900

    first

--- lib/develop/infra-develop-eks-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from '@aws-cdk/core';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as eks from '@aws-cdk/aws-eks';
-export declare class InfraDevelopEksStack extends cdk.Stack {
-    readonly vpc: ec2.IVpc;
-    readonly cluster: eks.ICluster;
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
-}
--- lib/infra-global-stack.d.ts
+++
@@ -1,6 +0,0 @@
-import * as cdk from '@aws-cdk/core';
-import * as route53 from '@aws-cdk/aws-route53';
-export declare class InfraGlobalStack extends cdk.Stack {
-    readonly route53: route53.IHostedZone;
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
-}
